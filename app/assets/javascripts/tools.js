0cb3fb393e16937068ffd9c6a179a455166e7032
Great_Divide <bbirdwell+git@gmail.com>
2019-01-24 10:58:53 -0800

    add listTool call to create_tool function, clean up code

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -116,17 +116,11 @@ $(function() {
 			$("#create_tool").trigger( "reset" ).attr('selected',false);
 
 			var i = $("#toolbox").children().length + 1
-			var id = tool.user_id
 
-			$("#toolbox").append(
-				`<div id="tool-${tool.id}">
-				<p>${i}. ` + 
-				tool.name + 
-				`  (${tool.description})  ` +
-				`<button id="show-tool-${tool.id}" data-user-id="${id}" data-tool-id="${tool.id}" onclick="showTool(${id}, ${tool.id})">' Show Tool '</button>
-				<button style="display: none" id="hide-tool-${tool.id}" onclick="hideTool(${tool.id})"> ' Hide Tool '</button></p>
-				<p id="tool-${tool.id}-status"></p>
-				</div>`);	
+			let newTool = new Tool(tool)
+
+			newTool.listTool(i)
+
 		})
 	})
 })
